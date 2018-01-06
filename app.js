c1058f8a0ffc75205c036954bd18fc3b66c880b5
b45i <amalshajan2011@gmail.com>
2018-01-06 12:09:55 +0530

    express api

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+
+const Aftership = require('aftership')('ab4fb55d-53ce-4bbe-992b-372249a71cd3');
+
+const app = express();
+var port = process.env.PORT || 3000;
+
+app.get('/', (request, response) => {
+    Aftership.call('GET', '/last_checkpoint/dhl/5398338002', function (err, result) {
+        if (err) {
+            return console.log(err);
+        }
+        let status = {
+            tracking_number: result.data['tracking_number'],
+            message: result.data.checkpoint['message']
+        };
+        response.send(status);
+    });
+});
+
+app.listen(3000, () => {
+    console.log(`App statred at ${port}`);
+});
